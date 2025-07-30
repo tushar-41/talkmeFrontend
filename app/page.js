f1ad4bdcb235a0f08a1048ec44dc722c147b6bd3
tushar-41 <tusharkmr4126@gmail.com>
2025-07-30 22:18:14 +0530

    buttons updated with link

--- app/page.js
+++ app/page.js
@@ -12,14 +12,13 @@ export default function Home() {
 
 
   useEffect(() => {
-    
   } ,[]);
 
 
   return (
     <div>
        <div className="min-h-screen bg-gradient-to-t from-blue-400 to-[#10172a] h-screen">
-        <div className="flex items-center justify-center fixed left-0 right-0 top-0 mt-10">
+        <div className="flex items-center justify-center fixed left-0 right-0 top-0 mt-10 z-100">
           <div className='bg-black/20 backdrop-blur-md h-12 w-1/2 rounded-4xl p-3 overflow-hidden'>
             <h1 className='font-bold font-mono text-white'>
              Talk Me
@@ -30,11 +29,21 @@ export default function Home() {
           <div className="font-mono bg-gradient-to-r from-red-700 to-teal-400 tracking-tighter text-7xl text-transparent bg-clip-text font-bold text-center mt-40">Welcome to Talkme</div>
           <p className="text-2xl text-muted-foreground text-black/80 p-10 w-200 text-center font-bold font-mono"> This website provide you the ability to chat with your friends in private space and with full privacy.</p>
         </div>
-        <div className="pt-10 flex items-center justify-center gap-20">
-          <button className="text-2xl border-none rounded-lg bg-gray-600 px-4 py-2 hover:bg-white/10 hover:cursor-pointer hover:scale-110 hover:text-black transition-all duration-300">Get started</button>
-          <Link href='#how it works'>
-          <button className="text-2xl border-none rounded-lg bg-gray-600 px-4 py-2 hover:bg-white/10 hover:cursor-pointer hover:scale-110 hover:text-black transition-all duration-300">See how it works</button>
-          </Link>
+        <div className="flex items-center justify-center gap-20">
+         <div className="relative inline-block rounded-xl p-[2px] overflow-hidden">
+          <div className="absolute inset-0 rounded-xl bg-[conic-gradient(red,orange,yellow,green,blue,indigo,violet,red)] animate-spin-slow z-0" />
+            <button className="relative z-10 px-6 py-2 bg-black text-white rounded-xl hover:bg-white hover:text-black hover:cursor-pointer transition-all duration-300">
+              Get started
+            </button>
+          </div>
+          <div className="relative inline-block rounded-xl p-[2px] overflow-hidden">
+          <div className="absolute inset-0 rounded-xl bg-[conic-gradient(red,orange,yellow,green,blue,indigo,violet,red)] animate-spin-slow z-0" />
+            <Link href={'#how it works'}>
+            <button className="relative z-10 px-6 py-2 bg-black text-white rounded-xl hover:bg-white hover:text-black hover:cursor-pointer transition-all duration-300">
+              How it's works
+            </button>
+            </Link>
+          </div>
         </div>
        </div>
        <section className="min-h-screen w-full bg-amber-700" id="how it works">
