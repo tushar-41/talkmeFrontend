33f901de5021539cd6a54452dedac36f2324c07c
tushar-41 <tusharkmr4126@gmail.com>
2025-08-13 23:17:27 +0530

    userContext initialized

---
+++ context/userContext.js
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react'
+import { createContext } from 'react';
+
+const userContext = () => {
+
+//user context functions
+
+  return (
+    <div>userContext</div>
+  )
+}
+
+export default userContext
